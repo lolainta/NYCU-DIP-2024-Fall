49e5ecf221d35e6f2a05bee874703489520238ef
LolaInTa Chi <lolainta@gmail.com>
2024-11-03 14:01:16 +0800

    hw2: style black format

--- hw2/image_enhancement.py
+++ hw2/image_enhancement.py
@@ -5,6 +5,8 @@ import numpy as np
 """
 TODO Part 1: Gamma correction
 """
+
+
 def gamma_correction():
     raise NotImplementedError
 
@@ -12,6 +14,8 @@ def gamma_correction():
 """
 TODO Part 2: Histogram equalization
 """
+
+
 def histogram_equalization():
     raise NotImplementedError
 
@@ -19,6 +23,8 @@ def histogram_equalization():
 """
 Bonus
 """
+
+
 def other_enhancement_algorithm():
     raise NotImplementedError
 
@@ -26,17 +32,22 @@ def other_enhancement_algorithm():
 """
 Main function
 """
+
+
 def main():
     img = cv2.imread("data/image_enhancement/input.bmp")
 
     # TODO: modify the hyperparameter
-    gamma_list = [1, 1, 1] # gamma value for gamma correction
+    gamma_list = [1, 1, 1]  # gamma value for gamma correction
 
     # TODO Part 1: Gamma correction
     for gamma in gamma_list:
         gamma_correction_img = gamma_correction()
 
-        cv2.imshow("Gamma correction | Gamma = {}".format(gamma), np.vstack([img, gamma_correction_img]))
+        cv2.imshow(
+            "Gamma correction | Gamma = {}".format(gamma),
+            np.vstack([img, gamma_correction_img]),
+        )
         cv2.waitKey(0)
 
     # TODO Part 2: Image enhancement using the better balanced image as input
--- hw2/image_restoration.py
+++ hw2/image_restoration.py
@@ -5,6 +5,8 @@ import numpy as np
 """
 TODO Part 1: Motion blur PSF generation
 """
+
+
 def generate_motion_blur_psf():
     raise NotImplementedError
 
@@ -12,6 +14,8 @@ def generate_motion_blur_psf():
 """
 TODO Part 2: Wiener filtering
 """
+
+
 def wiener_filtering():
     raise NotImplementedError
 
@@ -19,6 +23,8 @@ def wiener_filtering():
 """
 TODO Part 3: Constrained least squares filtering
 """
+
+
 def constrained_least_square_filtering():
     raise NotImplementedError
 
@@ -26,13 +32,20 @@ def constrained_least_square_filtering():
 """
 Bouns
 """
+
+
 def other_restoration_algorithm():
     raise NotImplementedError
 
 
 def compute_PSNR(image_original, image_restored):
     # PSNR = 10 * log10(max_pixel^2 / MSE)
-    psnr = 10 * np.log10(255 ** 2 / np.mean((image_original.astype(np.float64) - image_restored.astype(np.float64)) ** 2))
+    psnr = 10 * np.log10(
+        255**2
+        / np.mean(
+            (image_original.astype(np.float64) - image_restored.astype(np.float64)) ** 2
+        )
+    )
 
     return psnr
 
@@ -40,10 +53,16 @@ def compute_PSNR(image_original, image_restored):
 """
 Main function
 """
+
+
 def main():
     for i in range(2):
-        img_original = cv2.imread("data/image_restoration/testcase{}/input_original.png".format(i + 1))
-        img_blurred = cv2.imread("data/image_restoration/testcase{}/input_blurred.png".format(i + 1))
+        img_original = cv2.imread(
+            "data/image_restoration/testcase{}/input_original.png".format(i + 1)
+        )
+        img_blurred = cv2.imread(
+            "data/image_restoration/testcase{}/input_blurred.png".format(i + 1)
+        )
 
         # TODO Part 1: Motion blur PSF generation
         psf = generate_motion_blur_psf()
@@ -59,9 +78,15 @@ def main():
         print("PSNR = {}\n".format(compute_PSNR(img_original, wiener_img)))
 
         print("Method: Constrained least squares filtering")
-        print("PSNR = {}\n".format(compute_PSNR(img_original, constrained_least_square_img)))
+        print(
+            "PSNR = {}\n".format(
+                compute_PSNR(img_original, constrained_least_square_img)
+            )
+        )
 
-        cv2.imshow("window", np.hstack([img_blurred, wiener_img, constrained_least_square_img]))
+        cv2.imshow(
+            "window", np.hstack([img_blurred, wiener_img, constrained_least_square_img])
+        )
         cv2.waitKey(0)
 
 
