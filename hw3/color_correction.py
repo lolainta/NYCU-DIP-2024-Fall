6385972e11a81f738c840c584ae0eda418da7548
LolaInTa Chi <lolainta@gmail.com>
2024-12-02 06:39:17 +0800

    style: format string

--- hw3/color_correction.py
+++ hw3/color_correction.py
@@ -6,20 +6,26 @@ import os
 """
 TODO White patch algorithm
 """
-def white_patch_algorithm():
+
+
+def white_patch_algorithm(img):
     raise NotImplementedError
 
 
 """
 TODO Gray-world algorithm
 """
-def gray_world_algorithm():
+
+
+def gray_world_algorithm(img):
     raise NotImplementedError
 
 
 """
 Bonus 
 """
+
+
 def other_white_balance_algorithm():
     raise NotImplementedError
 
@@ -27,18 +33,25 @@ def other_white_balance_algorithm():
 """
 Main function
 """
+
+
 def main():
 
     os.makedirs("result/color_correction", exist_ok=True)
     for i in range(2):
-        img = cv2.imread("data/color_correction/input{}.bmp".format(i + 1))
+        img = cv2.imread(f"data/color_correction/input{i+1}.bmp")
 
         # TODO White-balance algorithm
         white_patch_img = white_patch_algorithm(img)
         gray_world_img = gray_world_algorithm(img)
 
-        cv2.imwrite("result/color_correction/white_patch_input{}.bmp".format(i + 1), white_patch_img)
-        cv2.imwrite("result/color_correction/gray_world_input{}.bmp".format(i + 1), gray_world_img)
+        cv2.imwrite(
+            f"result/color_correction/white_patch_input{i+1}.bmp", white_patch_img
+        )
+        cv2.imwrite(
+            f"result/color_correction/gray_world_input{i+1}.bmp", gray_world_img
+        )
+
 
 if __name__ == "__main__":
     main()
--- hw3/connected_component.py
+++ hw3/connected_component.py
@@ -6,27 +6,35 @@ import os
 """
 TODO Binary transfer
 """
-def to_binary():
+
+
+def to_binary(img):
     raise NotImplementedError
 
 
 """
 TODO Two-pass algorithm
 """
-def two_pass():
+
+
+def two_pass(img, connectivity):
     raise NotImplementedError
 
 
 """
 TODO Seed filling algorithm
 """
-def seed_filling():
+
+
+def seed_filling(img, connectivity):
     raise NotImplementedError
 
 
 """
 Bonus
 """
+
+
 def other_cca_algorithm():
     raise NotImplementedError
 
@@ -34,13 +42,17 @@ def other_cca_algorithm():
 """
 TODO Color mapping
 """
-def color_mapping():
+
+
+def color_mapping(label):
     raise NotImplementedError
 
 
 """
 Main function
 """
+
+
 def main():
 
     os.makedirs("result/connected_component/two_pass", exist_ok=True)
@@ -48,7 +60,7 @@ def main():
     connectivity_type = [4, 8]
 
     for i in range(2):
-        img = cv2.imread("data/connected_component/input{}.png".format(i + 1))
+        img = cv2.imread(f"data/connected_component/input{i+1}.png")
 
         for connectivity in connectivity_type:
 
@@ -58,13 +70,19 @@ def main():
             # TODO Part2: CCA algorithm
             two_pass_label = two_pass(binary_img, connectivity)
             seed_filling_label = seed_filling(binary_img, connectivity)
-        
-            # TODO Part3: Color mapping       
+
+            # TODO Part3: Color mapping
             two_pass_color = color_mapping(two_pass_label)
             seed_filling_color = color_mapping(seed_filling_label)
 
-            cv2.imwrite("result/connected_component/two_pass/input{}_c{}.png".format(i + 1, connectivity), two_pass_color)
-            cv2.imwrite("result/connected_component/seed_filling/input{}_c{}.png".format(i + 1, connectivity), seed_filling_color)
+            cv2.imwrite(
+                f"result/connected_component/two_pass/input{i+1}_c{connectivity}.png",
+                two_pass_color,
+            )
+            cv2.imwrite(
+                f"result/connected_component/seed_filling/input{i+1}_c{connectivity}.png",
+                seed_filling_color,
+            )
 
 
 if __name__ == "__main__":
